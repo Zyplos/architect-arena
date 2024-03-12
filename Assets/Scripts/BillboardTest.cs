a665fcda91c1b8d439c2247ed27c68dc18ab24b1
Angel <22824872+Zyplos@users.noreply.github.com>
2024-03-11 22:57:57 -0500

    drag and drop assets and scene

---
+++ Assets/Scripts/BillboardTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardTest : MonoBehaviour
+{
+    private Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    //  transform.LookAt(mainCamera.transform);
+
+    //  transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
+
+    Vector3 dir = transform.position - mainCamera.transform.position;
+ dir.y = 0.0F;
+ transform.rotation = Quaternion.LookRotation(dir);
+    }
+}
