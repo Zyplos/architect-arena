783a62fdc14fd4f802a47b5c37b6f85d1b23a401
flipkuzma <filipkuzma2002@gmail.com>
2024-04-09 22:10:56 -0500

    PuzzleRoom Audio

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -6,13 +6,16 @@ public class Door : MonoBehaviour
 {
     [SerializeField] private Animator myDoor = null;
     [SerializeField] private bool openTrigger = false;
-
+    public AudioSource source;
+    
     private void OnTriggerEnter(Collider other)
     {
+  
         if(other.CompareTag("Player"))
         {
             if (openTrigger)
             {
+                source.Play();
                 myDoor.Play("DoorOpen");
             }
         }
